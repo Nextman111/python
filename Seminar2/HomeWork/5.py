47172a55c811d4ee7195bc4c860d027495e126b5
VladislavRazvodov <g-man-111@ya.ru>
2022-12-01 00:50:38 +0300

    add 5.py

--- Seminar2/HomeWork/5.py
+++ Seminar2/HomeWork/5.py
@@ -5,8 +5,8 @@ from datetime import datetime
 def myRandom(a = -30, b = 30):
     return int((datetime.now().timestamp() % 1) * (b - a) + a)
 
-print(myRandom())
-
 # a = int(input('Введите левую границу '))
 # b = int(input('Введите правую границу '))
 # print(myRandom(a, b))
+
+print(myRandom())
